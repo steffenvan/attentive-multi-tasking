fb20e66391346d6b4d0fcf4f188b4af6a48eb7ba
steffenvan <steffem@vanfamily.com>
2019-04-14 15:05:18 +0200

    Removed redundant code

--- environments.py
+++ environments.py
@@ -157,55 +157,34 @@ class PyProcessAtari(object):
 
       self.num_action_repeats = num_action_repeats
       self._env = atari_wrappers.make_atari(env_id)
-      self._env = atari_wrappers.wrap_deepmind(self._env, frame_stack=True)
-      self.counter = 0
+      self._env = atari_wrappers.wrap_deepmind(self._env, frame_stack=False)
     def initial(self):
-
       initial_obs = self._env.reset()
-      # print
-      # print("(environments.py) initial obs: ", initial_obs[1])
-      # Uncomment to see the output. Should be a list of observations. 
-      # print("This is initial obs: ", initial_obs)
-      # TODO: This is awefully hard-coded. Removing the second argument in return, gives an error, because 
-      # it expects a tuple, where the second argument must be an empty string? 
-      # print("(environments.py) initial_shape_atari: ", initial_obs.shape)
-      self.counter = 0
       return initial_obs
     
     def render(self):
       return self._env.render()
 
     def step(self, action):
-      obs, reward, is_done, info = self._env.step(action)
-      # print("(environments.py) Type of obs: ", type(obs))
-      # print("(environments.py) Type of info: ", type(info))
-      # print("(environments.py) reward: {} type of reward: {} ".format(reward, type(reward)))
-      # reward = reward.astype(float)
-      # print ("(environments.py) Info is: ", info)
+      obs, reward, is_done, _ = self._env.step(action)
       self.counter += 1
       self._env.render()
       done = np.array(is_done)
       reward = np.float32(reward)
-      # print("is done info: ", done)
+      
       if done:
         self._env.reset() 
-      # info["ale.lives"]
-      # print("(environments.py) counter: {} - done is:{}".format(self.counter, done))
-      # if 
-      # print("(environments.py info is: ", info["ale.lives"])
 
       return reward, is_done, obs
 
     @staticmethod
     def _tensor_specs(method_name, unused_kwargs, constructor_kwargs):
       """Returns a nest of `TensorSpec` with the method's output specification."""
-      # print("Consturctor kwargs: ", constructor_kwargs)
+
       width = constructor_kwargs['config'].get('width', 210)
       height = constructor_kwargs['config'].get('height', 160)
-      # print("(environments.py) width: ", width)
-      # print("(environments.py) height: ", height)
 
-      observation_spec = tf.contrib.framework.TensorSpec([height, width, 3], tf.uint8)
+      observation_spec = tf.contrib.framework.TensorSpec([height, width, 1], tf.uint8)
 
       if method_name == 'initial':
         # print("(environments.py) obs_specs are: ", observation_spec)
