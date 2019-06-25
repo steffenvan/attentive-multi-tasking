8813fdb3e07dd0670fa64e611313d979491746af
steffenvan <steffem@vanfamily.com>
2019-06-25 22:26:42 +0200

    removed prints

--- atari_environment.py
+++ atari_environment.py
@@ -60,8 +60,6 @@ class PyProcessAtari(object):
 
     def __init__(self, env_id, config, seed):
       self._env = create_env(env_id)
-      print("HELLO: ", self._env.seed(seed))
-      print(self._env)
       
     def _reset(self):
       return self._env.reset()
