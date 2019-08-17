a9d0dc48e3aede9ca5dbd87cfd3c59d5391ad25a
steffenvan <steffem@vanfamily.com>
2019-08-17 18:52:28 +0200

    setting gpu options to 1/3 of the memory

--- atari_impala_vanilla.py
+++ atari_impala_vanilla.py
@@ -453,8 +453,9 @@ def train(action_set, level_names):
         
     # Create MonitoredSession (to run the graph, checkpoint and log).
     tf.logging.info('Creating MonitoredSession, is_chief %s', is_learner)
-    config = tf.ConfigProto(allow_soft_placement=True, device_filters=filters) 
-    config.gpu_options.allow_growth = True
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
+    config = tf.ConfigProto(allow_soft_placement=True, device_filters=filters, gpu_options=gpu_options) 
+    # config.gpu_options.allow_growth = True
     # config.gpu_options.per_process_gpu_memory_fraction = 0.8
     logdir = FLAGS.logdir
     with tf.train.MonitoredTrainingSession(
