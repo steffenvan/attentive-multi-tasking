00768bd0c134442c4e107e73e823268b98579cf6
steffenvan <steffem@vanfamily.com>
2019-09-07 12:39:36 +0200

    Cleaned up the agent file

--- agent.py
+++ agent.py
@@ -30,7 +30,7 @@ class ImpalaSubnet(snt.AbstractModule):
     self._num_actions = num_actions
     self._number_of_games = len(utilities_atari.ATARI_GAMES.keys())
     self.sub_networks = FLAGS.subnets
-    self.use_conv_attention = False
+    self.use_gap = FLAGS.use_gap
     self.use_separate_attn_net = FLAGS.use_separate_attn_net
 
   def _torso(self, input_):
@@ -60,8 +60,7 @@ class ImpalaSubnet(snt.AbstractModule):
         # Checks if attention needs to be calculated by this subnetwork.
         if not FLAGS.use_separate_attn_net or i == self.sub_networks:
           n_weights = 1 if not FLAGS.use_separate_attn_net else self.sub_networks
-          if self.use_conv_attention:
-          #   conv_attention = snt.Conv2D(1, 3, stride=1)(conv_out)
+          if self.use_gap:
             weight = tf.keras.layers.GlobalAveragePooling2D()(conv_out)
             weight = snt.Linear(n_weights, name='weights')(tf.concat([weight, tau], axis=1))
           else:
@@ -146,7 +145,7 @@ class SelfAttentionSubnet(snt.AbstractModule):
     self._num_actions = num_actions
     self._number_of_games = len(utilities_atari.ATARI_GAMES.keys())
     self.sub_networks = FLAGS.subnets
-    self.use_conv_attention = False
+    self.use_gap = FLAGS.use_gap
 
   def _torso(self, input_):
     last_action, env_output, level_name = input_
@@ -187,8 +186,7 @@ class SelfAttentionSubnet(snt.AbstractModule):
 
         conv_out = tf.nn.relu(conv_out)
         conv_out = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding="SAME")(conv_out)
-        if self.use_conv_attention:
-          #   conv_attention = snt.Conv2D(1, 3, stride=1)(conv_out)
+        if self.use_gap:
           weight = tf.keras.layers.GlobalAveragePooling2D()(conv_out)
           weight = snt.Linear(1, name='gap_weights')(tf.concat([weight, tau], axis=1))
         else:
@@ -220,12 +218,11 @@ class SelfAttentionSubnet(snt.AbstractModule):
 
   def _head(self, core_output):
     core_output, level_name = core_output
-    # Using a shared value function first.
-    # baseline_games = snt.Linear(1)(core_output)
-    # Then multiple value functions to account for the different scalings of rewards in different games.   
+
+    # A value function for each game. 
     baseline_games = snt.Linear(self._number_of_games)(core_output)
   
-    # adding time dimension
+    # Adding time dimension
     level_name     = tf.reshape(level_name, [-1, 1, 1])
     # Reshaping as to seperate the time and batch dimensions
     # We need to know the length of the time dimension, because it may differ in the initialization
@@ -261,74 +258,11 @@ class SelfAttentionSubnet(snt.AbstractModule):
 
     return snt.BatchApply(self._head)((torso_outputs, level_name))
 
-class ImpalaFeedForward(snt.AbstractModule):
-  """Agent with Simple CNN."""
-
-  def __init__(self, num_actions):
-    super(ImpalaFeedForward, self).__init__(name='impala_feed_forward_agent')
-
-    self._num_actions = num_actions
-
-  def _torso(self, input_):
-    last_action, env_output = input_
-    reward, _, _, frame = env_output
-
-    frame = tf.to_float(frame)
-    frame /= 255
-
-    with tf.variable_scope('convnet'):
-      conv_out = frame
-      conv_out = snt.Conv2D(16, 8, stride=4)(conv_out)
-      conv_out = tf.nn.relu(conv_out)
-      conv_out = snt.Conv2D(32, 4, stride=2)(conv_out)
-
-    conv_out = tf.nn.relu(conv_out)
-    conv_out = snt.BatchFlatten()(conv_out)
-    conv_out = snt.Linear(256)(conv_out)
-    conv_out = tf.nn.relu(conv_out)
-
-    # Append clipped last reward and one hot last action.
-    clipped_reward = tf.expand_dims(tf.clip_by_value(reward, -1, 1), -1)
-    one_hot_last_action = tf.one_hot(last_action, self._num_actions)
-    return tf.concat(
-        [conv_out, clipped_reward, one_hot_last_action],
-        axis=1)
-
-  def _head(self, core_output):
-    policy_logits = snt.Linear(self._num_actions, name='policy_logits')(core_output)
-    baseline = tf.squeeze(snt.Linear(1, name='baseline')(core_output), axis=-1)
-
-    # Sample an action from the policy.
-    new_action = tf.multinomial(policy_logits, num_samples=1,
-                                output_dtype=tf.int32)
-    new_action = tf.squeeze(new_action, 1, name='new_action')
-
-    return ImpalaAgentOutput(new_action, policy_logits, baseline)
-
-  def _build(self, input_):
-    action, env_output = input_
-    actions, env_outputs = nest.map_structure(lambda t: tf.expand_dims(t, 0),
-                                              (action, env_output))
-    outputs = self.unroll(actions, env_outputs)
-    return nest.map_structure(lambda t: tf.squeeze(t, 0), outputs)
-
-  @snt.reuse_variables
-  def unroll(self, actions, env_outputs):
-    _, _, done, _ = env_outputs
-
-    torso_outputs = snt.BatchApply(self._torso)((actions, env_outputs))
-
-    return snt.BatchApply(self._head)(torso_outputs)
-
 
 def agent_factory(agent_name):
   specific_agent = {
     'ImpalaSubnet'.lower(): ImpalaSubnet,
-    'SelfAttentionSubnet'.lower(): SelfAttentionSubnet,
-    'ImpalaFeedForward'.lower(): ImpalaFeedForward,
-    # 'PopArtFeedForward'.lower(): PopArtFeedForward,
-    # 'ImpalaLSTM'.lower(): ImpalaLSTM,
-    # 'PopArtLSTM'.lower(): PopArtLSTM
-  }
+    'SelfAttentionSubnet'.lower(): SelfAttentionSubnet
+   }
 
   return specific_agent[agent_name.lower()]
